8cbcca58bccf25cd4b4463c6fefcd0924d5dc4a2
Cumen0 <tvarfamasi228@gmail.com>
2025-12-09 14:44:57 +0100

    Fix: Allow HTTPS traffic in Security Group

--- aws-ecs-infra/modules/alb/main.tf
+++ aws-ecs-infra/modules/alb/main.tf
@@ -1,13 +1,21 @@
 # --- Security Group for ALB ---
 resource "aws_security_group" "alb_sg" {
   name        = "${var.project_name}-${var.env}-alb-sg"
-  description = "Allow HTTP inbound traffic"
+  description = "Allow HTTP and HTTPS inbound traffic"
   vpc_id      = var.vpc_id
 
   ingress {
     description = "HTTP from Internet"
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS from Internet"
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
